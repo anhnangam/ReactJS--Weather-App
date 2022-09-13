6c8519fd9b92e781388b8c8e23431fbd4fb38113
DucManh <hoangnam10262000@gmail.com>
2022-09-13 16:17:58 +0700

    upload source code

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d29fa677f6msh8877161ce8eb5d9p13a7a3jsn029c2a19ca8d",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "6a1828087224c0a5ca101f42f87b4973"; // enter your key from openweather API
